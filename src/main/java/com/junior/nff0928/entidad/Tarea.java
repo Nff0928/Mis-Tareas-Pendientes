4f31d791140194649bc9e77c95add17c14907fa3
Nicolasff12 <nfonsecaf2@ucentral.edu.co>
2024-12-28 22:33:18 -0500

    Crear mis tareas, falta grafica

---
+++ src/main/java/com/junior/nff0928/entidad/Tarea.java
@@ -0,0 +1,68 @@
+package com.junior.nff0928.entidad;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+    private String nombre;
+    private String descripcion;
+    private boolean estado = false;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+
+
+}
